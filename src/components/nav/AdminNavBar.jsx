fddcbc8d609f4b95e3e08615876ee8ecbad3d58b
ASM <Alexanderstubmichelsen@gmail.com>
2023-07-26 13:35:31 +0200

    Revert "merge commit"
    This reverts commit 8db6825859c4aa4adfb300fcf6a23eebb803d7bb.

--- src/components/nav/AdminNavBar.jsx
+++ src/components/nav/AdminNavBar.jsx
@@ -3,16 +3,9 @@ import NavItem from "./NavItem";
 export default function AdminNavBar({ loggedIn, user }) {
 
     return (
-        <>
         <ul className="navBar">
-            <b>&nbsp;&nbsp;&nbsp;&nbsp;This is the admin navigation bar</b>
-            </ul>
-        <ul className="navBar">  
-            <NavItem to="AllJokes" text="All jokes" />
-            <NavItem to="Jokes" text="Iterate jokes" />
-            <NavItem to="JokesByCat" text="Category based jokes" />
-            <NavItem to="CreateJoke" text="Create joke" />
+            <NavItem to="allJokes" text="All jokes" />
+            <NavItem to="jokes" text="Iterate jokes" />
         </ul>
-        </>
     );
 }
